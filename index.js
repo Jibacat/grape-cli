298c5dc10a09c7128372201369b5c8be62e90ca1
Jibacat <wfwangchengqian@126.com>
2019-03-18 17:35:56 +0800

    添加typescript parser

--- index.js
+++ index.js
@@ -86,6 +86,12 @@ let clientRoadmap = {
         }),
         rExt: '.js'
     },
+    'client/**.ts': {
+        parser: fis.plugin('typescript', {
+                target: 1
+            }),
+        rExt: '.js'
+    },
     'client/**.vue' : {
         isMod: true,
         rExt: 'js',
